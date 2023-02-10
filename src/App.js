16836f0ca175971c73b7d740e91ca528797aaaa0
Sainikhil <sainikhil1885007@gmail.com>
2023-02-10 09:15:08 +0530

    First upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import NotesList from "./components/NotesList";
+import { useState } from "react";
+import {nanoid} from 'nanoid';
+import AddNote from "./components/AddNotes";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const[notes,setNotes] = useState([{
+        id :  nanoid(),
+        text:"This is my first note !",
+        date: "7/02/2023",
+  },
+  {
+    id :  nanoid(),
+    text:"This is my second note !",
+    date: "8/02/2023",
+},
+{
+  id :  nanoid(),
+  text:"This is my third note !",
+  date: "9/02/2023",
+},
+{
+  id :  nanoid(),
+  text:"This is my fourth note !",
+  date: "10/02/2023",
+},
+]);
+const [searchText,setSearchText] = useState('');
+
+ const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id : nanoid(),
+      text : text,
+      date : date.toLocaleDateString,
+    }
+    const newNotes = [...notes,newNote];
+    setNotes(newNotes)
+ }; 
+ const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+ }
+  return ( <div className="container">
+    <Header/>
+    <Search handleSearchNote = {setSearchText}/>
+    <NotesList notes={notes.filter((note)=> note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
+  </div> );
+};
 
 export default App;
